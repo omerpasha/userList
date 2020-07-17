1fec4e8c2ce251d19dee4efbe1b7b087dcaed23c
Omer Yalap <oyalap@broadcast.local>
2020-07-17 16:41:57 +0300

    delete funtion and login are  added

--- app.js
+++ app.js
@@ -1,55 +1,52 @@
-
+/*gereklilikler eklendi*/ 
 var express = require('express');
 var app = express();
 var fs=require('fs');
 url=require('url');
 var cors = require('cors');
 var path = require('path');
-app.use(cors());
+app.use(cors());//cors hatasını gidermek için
 const bodyParser = require('body-parser');
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
-
+/*kullanıcıları listelemek için*/
 app.get('/listele', function (req, res) {
-    fs.readFile('users.json','utf-8',function(err,data){
+     fs.readFile('users.json','utf-8',function(err,data){
         res.send(data);
         console.log(data);
 
     });
 });
+app.delete('/delete',function(req,res){
+
+        console.log(req.body);
+});
+
 //ekle
-app.post('/ekle', function (req, res) {   
+app.post('/kullaniciEkle', function (req, res) {   
     console.log(req.body);
 });
-    //console.log('ekleye geldi')
-    // const yeni_kullanici={
-    //     "k3":{
-    //         "isim":"besim timuk",
-            
-    //         "tc":1321,
+    
+app.get('/ekle',function(req,res){
+    res.sendFile(path.join(__dirname + '\\ekle.html'));
+});
 
-    //         "email":"besim@ahaha"
-    //     }
-    // }
-    //const data = JSON.stringify(yeni_kullanici);
+app.get('/login',function(req,res){
+   
+    res.sendFile(path.join(__dirname + '\\login.html'));
 
-    // write JSON string to a file
-    // fs.writeFile('users.json', data, (err) => {
-    //     if (err) {
-    //         throw err;
-    //     }
-    //     console.log("JSON data is saved.");
-    // });
+});
 
 
 
-
+//girdiğinde direk root olarak alsın diye
 
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '\\index.html'));
 });
-
+//8090 portunu dinlemeye aldık
 app.listen(8090,function(){    
     console.log('sunucu calisiyor');
 });

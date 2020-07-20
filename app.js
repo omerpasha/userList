d51f92a1d0f4a5dc62614c1591c245da7912f834
Omer Yalap <oyalap@broadcast.local>
2020-07-20 16:20:38 +0300

    editting

--- app.js
+++ app.js
@@ -2,11 +2,18 @@
 var express = require('express');
 var app = express();
 var fs=require('fs');
-url=require('url');
+var url=require('url');
 var cors = require('cors');
 var path = require('path');
+
+
+
+
 app.use(cors());//cors hatasını gidermek için
 const bodyParser = require('body-parser');
+const { json } = require('body-parser');
+
+ var users;
 
 
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -17,6 +24,8 @@ app.get('/listele', function (req, res) {
         res.send(data);
         console.log(data);
 
+       users=data;
+
     });
 });
 app.delete('/delete',function(req,res){
@@ -24,7 +33,37 @@ app.delete('/delete',function(req,res){
         console.log(req.body);
 });
 
-//ekle
+app.put('/kullaniciDuzenle',function(req,res){
+   
+    console.log(req.param.id);
+
+});
+
+ app.get('/kullaniciGetir/:id',function(req,res){
+    
+    var id=req.params.id;
+    
+    fs.readFile('users.json','utf-8',function(err,data){
+        var records=[];
+        records=JSON.parse(fs.readFileSync('users.json','utf-8'));
+        console.log(records);
+        console.log(records.length);
+
+            for(var i=0;i<records.length;i++){
+                if(records[i].tc==id){
+                    console.log('kullanici'+id);
+                    break ;
+                }
+                else{
+                    console.log('kullanici bulunamadi');
+                }
+
+            }
+        });
+    });
+
+
+
 app.post('/kullaniciEkle', function (req, res) {   
     console.log(req.body);
 });
@@ -39,6 +78,13 @@ app.get('/login',function(req,res){
 
 });
 
+app.get('/edit/:id',function(req,res){
+    //yönlednirme id ile olan
+    res.sendFile(path.join(__dirname+'\\edit.html'));//dosya adı
+
+});
+
+
 
 
 //girdiğinde direk root olarak alsın diye
@@ -46,7 +92,10 @@ app.get('/login',function(req,res){
 app.get('/', function (req, res) {
     res.sendFile(path.join(__dirname + '\\index.html'));
 });
+
+
 //8090 portunu dinlemeye aldık
 app.listen(8090,function(){    
     console.log('sunucu calisiyor');
 });
+
